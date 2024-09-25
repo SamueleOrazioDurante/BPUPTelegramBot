725318beefdc02405803885d24c3f988fd2d155c
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-09-25 09:44:00 +0000

    raw_data_bot mention

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = ''  # your bot token from BotFather
 TIKTOK_API_TOKEN = ''  # https://rapidapi.com/yi005/api/tiktok-download-without-watermark
 TWITTER_API_TOKEN = ''  # https://tweethunter.io/tweetpik
-CHAT_ID = '' # bot will only answer to this chat id (set it to -1 if you want it to be able to answer to anyone)
+CHAT_ID = '' # bot will only answer to this chat id (set it to -1 if you want it to be able to answer to anyone). Use (https://t.me/raw_data_bot)
