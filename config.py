619386009435d1635da2fa97059404de16f1f9e9
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-09-25 11:37:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 BOT_TOKEN = ''  # your bot token from BotFather
 TIKTOK_API_TOKEN = ''  # https://rapidapi.com/yi005/api/tiktok-download-without-watermark
 TWITTER_API_TOKEN = ''  # https://tweethunter.io/tweetpik
+CHAT_ID = ''
