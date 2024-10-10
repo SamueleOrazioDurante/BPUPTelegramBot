2bc97b5a3fd216df52d0ebc91c2b2e5386da0343
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-10-10 21:41:04 +0000

    model changed pt2

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,18 @@ WORKDIR /usr/src/app
 
 RUN apt update && apt install -y \
         ffmpeg \
-        flac
+        flac \
+        unzip \
+        wget
 
 COPY requirements.txt .
 
 RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
+
+RUN wget "https://alphacephei.com/vosk/models/vosk-model-it-0.22.zip"
+RUN unzip vosk-model-it-0.22.zip
+RUN rm vosk-model-it-0.22.zip
 
 COPY . .
 
--- voiceRecognizer.py
+++ voiceRecognizer.py
@@ -1,30 +1,47 @@
 import os
 import subprocess
 
-from transformers import pipeline
+import json
+import wave
+from vosk import Model, KaldiRecognizer, SetLogLevel
 
-# Crea il pipeline per il riconoscimento automatico del parlato
-pipe = pipeline("automatic-speech-recognition", model="ALM/whisper-it-small")
+# Set log level to 0 to suppress Vosk logs
+SetLogLevel(0)
+
+# Load the Italian model
+model_path = "vosk-model-it-0.22"  # Change this to your model path
+
+model = Model(model_path)
+recognizer = KaldiRecognizer(model, 16000)
+
 
 def voice_recognizer(language):
 
-    subprocess.run(['ffmpeg', '-i', 'audio.ogg', 'audio.wav', '-y'])  # formatting ogg file in to wav format
+    subprocess.run(['ffmpeg', '-i', 'audio.ogg', '-ar', '16000', '-ac', '1', '-c:a', 'pcm_s16le', 'audio.wav', '-y'])  # formatting ogg file in to wav format
     text = 'Words not recognized.'
-
-
+    
     try:
 
-        # Specifica il percorso del file audio
-        audio_file = "audio.wav"
+        # Open a WAV file (you can also modify this to use a microphone)
+        wf = wave.open("audio.wav", "rb")  # Change to your audio file path
+        if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getframerate() != 16000:
+            print("Audio file must be WAV format mono PCM.")
+            return text
 
-        # Trasforma il file audio in testo
-        result = pipe(audio_file)
-
-        # Stampa il risultato
-        text = result['text']
+        # Read audio data and transcribe
+        while True:
+            data = wf.readframes(4000)
+            if len(data) == 0:
+                break
+            if recognizer.AcceptWaveform(data):
+                result = recognizer.Result()
+                text = json.loads(result).get("partial", "")
+            else:
+                text = json.loads(recognizer.PartialResult()).get("partial", "")
 
     except Exception as e:
         print(str(e))
+        clear()
 
     return text
 
