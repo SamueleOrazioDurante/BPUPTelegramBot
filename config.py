cda6da2d43d79bc8befff3e0e94f01321369a76a
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-10-10 18:12:04 +0000

    me son dimenticato l`apikey :D (l'ho già resettata chill)

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = '7283550566:AAGpG3kZ1voS0YeUixK2T-uswBbZZl3R_6s'  # your bot token from BotFather
-TIKTOK_API_TOKEN = 'a'  # https://rapidapi.com/yi005/api/tiktok-download-without-watermark
-TWITTER_API_TOKEN = 'a'  # https://tweethunter.io/tweetpik
-CHAT_ID = '334036736' # bot will only answer to this chat id (set it to -1 if you want it to be able to answer to anyone). Use (https://t.me/raw_data_bot)
+BOT_TOKEN = ''  # your bot token from BotFather
+TIKTOK_API_TOKEN = ''  # https://rapidapi.com/yi005/api/tiktok-download-without-watermark
+TWITTER_API_TOKEN = ''  # https://tweethunter.io/tweetpik
+CHAT_ID = '' # bot will only answer to this chat id (set it to -1 if you want it to be able to answer to anyone). Use (https://t.me/raw_data_bot)
