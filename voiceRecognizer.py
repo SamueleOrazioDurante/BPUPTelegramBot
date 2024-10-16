4f19224d8c3bd26bd25469592ad422d4a2329388
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-10-16 21:19:47 +0000

    readded ffmpeg

--- voiceRecognizer.py
+++ voiceRecognizer.py
@@ -8,13 +8,18 @@ whisper_model = whisper.load_model("small") #+ ("." + "it"))
 
 def voice_recognizer(language):
 
-    result = whisper_model.transcribe("audio.wav", verbose=False, language=language, fp16=False)
-    rawtext = " ".join([segment["text"].strip() for segment in result["segments"]])  # type: ignore
-    rawtext = re.sub(" +", " ", rawtext)
-    alltext = re.sub(r"([\.\!\?]) ", r"\1\n", rawtext)
+    subprocess.run(['ffmpeg', '-i', 'audio.ogg', 'audio.wav', '-y'])  # formatting ogg file in to wav format
+
+    try:
+        result = whisper_model.transcribe("audio.wav", verbose=False, language=language, fp16=False)
+        rawtext = " ".join([segment["text"].strip() for segment in result["segments"]])  # type: ignore
+        rawtext = re.sub(" +", " ", rawtext)
+        alltext = re.sub(r"([\.\!\?]) ", r"\1\n", rawtext)
+    except:
+        clear()
+        return "Errore, riprova più tardi."
 
     clear()
-
     return alltext
 
 
