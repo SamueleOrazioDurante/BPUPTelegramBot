d7b1de70ecac40b20a3719c9a085bab8f2054e50
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-10-17 21:25:45 +0000

    api key leak ups (already changed :P)

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-BOT_TOKEN = '7283550566:AAFNkeO7pxw7pDsgshZ6SgJfXlJhKbWmbRU'  # your bot token from BotFather
-TIKTOK_API_TOKEN = 'df'  # https://rapidapi.com/yi005/api/tiktok-download-without-watermark
-TWITTER_API_TOKEN = 'gfd'  # https://tweethunter.io/tweetpik
-CHAT_ID = '334036736' # bot will only answer to this chat id (set it to -1 if you want it to be able to answer to anyone). Use (https://t.me/raw_data_bot)
+BOT_TOKEN = ''  # your bot token from BotFather
+TIKTOK_API_TOKEN = ''  # https://rapidapi.com/yi005/api/tiktok-download-without-watermark
+TWITTER_API_TOKEN = ''  # https://tweethunter.io/tweetpik
+CHAT_ID = '' # bot will only answer to this chat id (set it to -1 if you want it to be able to answer to anyone). Use (https://t.me/raw_data_bot)
 
 # IA Transcription
-LANGUAGE = "italian" # language used for recognization (italian,english,spanish,...)
-MODEL = "small" # (small,large)
+LANGUAGE = "" # language used for recognization (italian,english,spanish,...)
+MODEL = "" # (small,large)
