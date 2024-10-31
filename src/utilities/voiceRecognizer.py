ef840a8ff4dba07ba9e0745bbfcdf3cc5c00af5b
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-10-31 10:35:00 +0000

    exception fixed in voiceRecognizer

--- src/utilities/voiceRecognizer.py
+++ src/utilities/voiceRecognizer.py
@@ -26,8 +26,8 @@ def voice_recognizer(wav_audio_path):
 
     try:
         text += pipe(wav_audio_path,generate_kwargs={"language": f"{language}"})["text"]
-    except:
-        text = "Errore, riprova più tardi."
+    except Exception:
+        text = "Errore, riprova più tardi:"+ str(Exception)
 
     clear(wav_audio_path)
     return text
